d1748cb473f2e0994d01b878b206574cb247f5df
emkyu404 <contact.buiminhquan@gmail.com>
2022-09-13 17:03:15 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+
+#include <iostream>
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() 
+{
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+
+	// Ouvre une fenêtre et crée son contexte OpenGl
+	GLFWwindow* window; // (Dans le code source qui accompagne, cette variable est globale)
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Initialise GLEW
+	glewExperimental = true; // Nécessaire dans le profil de base
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	IMGUI_CHECKVERSION();
+
+	// Assure que l'on peut capturer la touche d'échappement enfoncée ci-dessous
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		// Ne dessine rien, on se voit dans le tutoriel 2 !
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Vérifie si on a appuyé sur la touche échap (ESC) ou si la fenêtre a été fermée
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	return 0;
+}
