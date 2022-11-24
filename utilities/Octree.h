dd2390220e509ab18258b797b0dab462f67fc2fd
Jade803 <jade.cothenet@etu.univ-lyon1.fr>
2022-11-24 10:33:02 -0500

    Correct include vcx

---
+++ utilities/Octree.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+#include "Node.h"
+#include "Vector3D.hpp"
+#include "RigidBody.h"
+
+class Octree {
+
+private :
+
+	vector<Node*> listNodes;
+
+public :
+
+	/*-------------- CONSTRUCTORS --------------*/
+
+	Octree();
+	Octree(vector<Node*> _listNodes);
+
+	/*-------------- GETTERS --------------*/
+
+	vector<Node*> getNodes();
+
+	/*-------------- METHODS --------------*/
+
+	Node* createOctree(Vector3D _center, float _halfWidth, int _stopDepth);
+	void insertRb(Node* _pTree, RigidBody* _rigidBody);
+};
+
